7acc4e1cd32cec76fedb4b0f4870f63beb9bdb78
username <email>
2023-01-15 16:48:52 +0800

    优化mock目录结构

--- mock/_mockProdServer.ts
+++ mock/_mockProdServer.ts
@@ -1,14 +1,14 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
 
-// 逐一导入您的mock.ts文件
-// 如果使用vite.mock.config.ts，只需直接导入文件
+// 逐一导入 mock.ts 文件
+// 如果使用 vite.mock.config.ts，只需直接导入文件
 // 可以使用 import.meta.glob功能来进行全部导入
 // import adminModule from './modules/admin';
 // export function setupProdMockServer() {console.log('mock注入');
 //   createProdMockServer([...adminModule]);
 // }
 
-const modules = import.meta.glob('./**/*.ts', {
+const modules = import.meta.glob('./modules/**/*.ts', {
   import: 'default',
   eager: true
 });
